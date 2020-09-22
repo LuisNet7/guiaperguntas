f587cd711b5f8d0cc61611936d97f48cc9088d04
luis alberto J.S <luis.albertosax@gmail.com>
2020-09-22 12:18:58 -0300

    guia de perguntas e respostas em node js

---
+++ database/database.js
@@ -0,0 +1,11 @@
+const Sequelize = require('sequelize');
+
+const connection = new Sequelize('guiapergunta','root','',{
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
+module.exports = connection;
+
+
+
